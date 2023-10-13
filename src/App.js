77abe41d6109a480cbbf9ffa222fa060be178ef1
uladzimir <01myit@gmail.com>
2023-10-13 20:22:36 +0300

    state concept in class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import styles from './App.module.css' 
 import Button from './UI/Button'
 import Customers from './components/Customers'
-import { useState } from 'react'
-function App() {
-  const [hiding, setHiding] = useState(false)
-  function toggleHiding() {
-    setHiding(hiding => !hiding)
+import { Component } from 'react'
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      hiding: false,
+    }
   }
-  return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <h1>Hi, Classes, in Recat!</h1>
-      </header>
-      <Button className={styles.button}
-        foo={toggleHiding}
-      >Toggle customer hiding</Button>
-      {
-        hiding && <Customers className={styles.customers} />
-      }
-    </div>
-  ) 
+  toggleHiding() {
+    this.setState((state) => {
+      return { hiding: !state.hiding}
+    })
+  }
+  render() {
+      return (
+        <div className={styles.App}>
+          <header className={styles['App-header']}>
+            <h1>Hi, Classes, in Recat!</h1>
+          </header>
+          <Button className={styles.button}
+            foo={this.toggleHiding.bind(this)}
+          >Toggle customer hiding</Button>
+          {
+            this.state.hiding && <Customers className={styles.customers} />
+          }
+        </div>
+      ) 
+    }
 }
 
 export default App 
